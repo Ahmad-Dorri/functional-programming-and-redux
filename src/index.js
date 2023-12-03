378fac102b4858ca984d87cb7d0488760c3252d5
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-12-03 18:54:47 +0330

    function composition and using lodash library for making things cleaner

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+//! FUNCTION COMPOSITION
+import { compose, pipe } from 'lodash/fp';
 const trim = (str) => str.trim();
 const wrapInDiv = (str) => `<div>${str}</div>`;
 const toLowerCase = (str) => str.toLowerCase();
 
 const input = '   Javascript   ';
 
+// USING COMPOSE FROM LODASH FOR MAKING THINGS CLEANER
+const transform = compose(wrapInDiv, toLowerCase, trim);
+const composeResult = transform(input);
+
+//FUNCTION COMPOSITION
 const result = wrapInDiv(toLowerCase(trim(input)));
+
+console.log(composeResult, result);
